2d143c9859a71eaef9af833468aa944f28279a5d
oalkada <oalkada@gmail.com>
2019-09-19 12:38:05 -0400

    initial commit

---
+++ src/main/java/ouri/Calculate.java
@@ -0,0 +1,18 @@
+package ouri;
+
+public class Calculate {
+	public float hoursWorked;
+	public float hourlyWage;
+	public float weeklyPay;
+	
+	
+	public Calculate(float hourlyWage, float hoursWorked,float weeklyPay) {
+		super();
+		this.hourlyWage = hourlyWage;
+		this.hoursWorked = hoursWorked;
+		this.weeklyPay=weeklyPay;
+	}
+
+	
+	
+}
